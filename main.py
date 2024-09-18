ce212d1e3a5992c5cd5d2b25946f313ff1e450a6
Milena Maya Torrico Santiestevez <153246370+milets04@users.noreply.github.com>
2024-09-18 00:19:27 -0400

    Sumar

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ def dividir_numeros(dividendo, divisor):
     except ZeroDivisionError:
         return "Error: No se puede dividir entre cero."
 # María Laura Fernández Campos
+# Milena Maya Torrico Santiestevez
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+resultado = num1 + num2
+print(f"El resultado de la suma es: {resultado}")
