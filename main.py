9b520d79896bbb1c9fd98bd796cdd46ff62e6f5a
floramos06 <202200303@est.umss.edu>
2024-09-18 21:08:10 -0400

    Commit resta

--- main.py
+++ main.py
@@ -37,3 +37,18 @@ def calcular_raiz(numero):
         return "No se puede calcular la raíz cuadrada de un número negativo"
     else:
         return math.sqrt(numero)
+
+#Florencia Ramos Gamarra
+#Resta
+def resta(a, b):
+    return a - b
+
+try:
+    numero1 = float(input("Introduce el primer número: "))
+    numero2 = float(input("Introduce el segundo número: "))
+
+    resultado = resta(numero1, numero2)
+    print(f"La resta de {numero1} y {numero2} es: {resultado}")
+    
+except ValueError:
+    print("Por favor, introduce un número válido.")
