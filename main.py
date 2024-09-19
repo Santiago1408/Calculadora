c43611a39fd38905c4b5e7d41318192a70d2e5c2
marielena21 <82038959+marielena21@users.noreply.github.com>
2024-09-18 21:05:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("Seleccione la operación que desea realizar: \n 1. Suma \n 2. Resta \n 3. Multiplicación \n 4. División")
 
 operacion = input("Opereación: ")
-
+import math
 #Josue Garcia
 def mult(num1, num2):
     return num1 * num2
@@ -28,3 +28,12 @@ def calcular_exponencial(base, exponente):
     resultado = base ** exponente
     print(f"{base}^{exponente} = {resultado}")
     return resultado
+
+
+#funcion para sacar raiz de un numero 
+#Maria Elena Bernabe
+def calcular_raiz(numero):
+    if numero < 0:
+        return "No se puede calcular la raíz cuadrada de un número negativo"
+    else:
+        return math.sqrt(numero)
